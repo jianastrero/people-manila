637a7934ae8a8965e5c327a3b9fd1eb8669ef982
Jian James Astrero <jianjamesastrero@gmail.com>
2024-08-31 18:23:45 +0800

    🎨 StyleSheet

--- App.js
+++ App.js
@@ -1,16 +1,53 @@
 import React from 'react';
 import {
-    Alert,
+    Button,
+    Image,
     SafeAreaView,
+    ScrollView,
     StatusBar,
-    StyleSheet,
+    StyleSheet, Switch,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    View
 } from 'react-native';
 
 export default function App() {
-    Alert.alert('Hi, Alert', 'This is a message for the alert!');
+    const [value, setValue] = React.useState('');
+    const [switchValue, setSwitchValue] = React.useState(false);
+
     return (
-        <SafeAreaView>
+        <SafeAreaView style={[styles.container, styles.widthFull]}>
             <StatusBar backgroundColor="black" />
+            <ScrollView style={styles.widthFull}>
+                <View style={styles.redBlock} />
+                <Text style={styles.orangeText}>Hello, World!</Text>
+                <Image
+                    source={{ uri: 'https://placedog.net/720/520' }}
+                    style={styles.bigImage}/>
+                <TextInput
+                    value={value}
+                    placeholder="placeholder"
+                    onChangeText={newValue => setValue(newValue)}
+                    style={styles.textInput}/>
+                <Button
+                    title='My Button'
+                    color='magenta'
+                    onPress={() => {}} />
+                <TouchableOpacity
+                    onPress={() => {}}
+                    style={styles.touchableButton}>
+                    <Text>Hello World!</Text>
+                </TouchableOpacity>
+                <Switch
+                    value={switchValue}
+                    onValueChange={newSwitchValue => setSwitchValue(newSwitchValue)}
+                    thumbColor='red'
+                    trackColor={{
+                        true: 'blue',
+                        false: 'green'
+                    }}/>
+            </ScrollView>
         </SafeAreaView>
     );
 }
@@ -22,4 +59,35 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
     },
+    widthFull: {
+        width: '100%'
+    },
+    redBlock: {
+        width: 100,
+        height: 100,
+        backgroundColor: 'red'
+    },
+    orangeText: {
+        fontWeight: 'bold',
+        fontSize: 24,
+        fontStyle: 'italic',
+        color: 'orange'
+    },
+    bigImage: {
+        width: '100%',
+        height: 520
+    },
+    textInput: {
+        paddingHorizontal: 24,
+        paddingVertical: 12,
+        borderColor: 'black',
+        borderWidth: 1,
+        borderRadius: 16
+    },
+    touchableButton: {
+        paddingHorizontal: 24,
+        paddingVertical: 12,
+        borderRadius: 16,
+        backgroundColor: 'yellow'
+    }
 });
