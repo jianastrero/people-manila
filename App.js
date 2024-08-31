3ade8b95bc8f00c98b47719da35ce042033ffee9
Jian James Astrero <jianjamesastrero@gmail.com>
2024-08-31 18:03:02 +0800

    🔃 ActivityIndicator

--- App.js
+++ App.js
@@ -1,86 +1,19 @@
 import React from 'react';
 import {
-    FlatList,
-    SafeAreaView, SectionList,
+    ActivityIndicator,
+    SafeAreaView,
+    SectionList,
     StatusBar,
     StyleSheet, Text
 } from 'react-native';
-
-const DATA = [
-    {
-        title: 'Manila',
-        data: [
-            'Makati',
-            'Quezon City',
-            'Pasig',
-            'Taguig',
-            'Mandaluyong'
-        ]
-    },
-    {
-        title: 'Cebu',
-        data: [
-            'Cebu City',
-            'Lapu-Lapu City',
-            'Mandaue City',
-            'Talisay City',
-            'Danao City'
-        ]
-    },
-    {
-        title: 'Davao',
-        data: [
-            'Davao City',
-            'Panabo City',
-            'Tagum City',
-            'Island Garden City of Samal',
-            'Digos City'
-        ]
-    },
-    {
-        title: 'Cagayan de Oro',
-        data: [
-            'Cagayan de Oro City',
-            'El Salvador City',
-            'Gingoog City',
-            'Iligan City',
-            'Ozamiz City'
-        ]
-    },
-    {
-        title: 'Baguio',
-        data: [
-            'Baguio City',
-            'La Trinidad',
-            'Itogon',
-            'Sablan',
-            'Tuba'
-        ]
-    }
-];
 
 export default function App() {
     return (
         <SafeAreaView>
             <StatusBar backgroundColor="black" />
-            <SectionList
-                sections={DATA}
-                renderSectionHeader={({section: {title}}) => (
-                    <Text style={{
-                        padding: 12,
-                        color: 'white',
-                        backgroundColor: 'blue',
-                        fontSize: 32,
-                        fontWeight: 'bold'
-                    }}>{title}</Text>
-                )}
-                renderItem={({item}) => (
-                    <Text style={{
-                        padding: 12,
-                        borderBottomWidth: 1,
-                        borderBottomColor: 'orange'
-                    }}>{item}</Text>
-                )} />
+            <ActivityIndicator
+                color='blue'
+                size='large' />
         </SafeAreaView>
     );
 }
