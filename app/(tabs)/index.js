5f9dc1252130f2e7f139fce2c8b657b9bb30a252
Jian James Astrero <jianjamesastrero@gmail.com>
2024-09-01 08:12:15 +0800

    🤏 Introduction Animations

--- app/(tabs)/index.js
+++ app/(tabs)/index.js
@@ -2,48 +2,96 @@ import React from 'react';
 import {StatusBar, View} from 'react-native';
 import {myTheme, colors} from "../../src/theme/MyTheme";
 import {Gesture, GestureDetector, GestureHandlerRootView} from "react-native-gesture-handler";
-import Animated from 'react-native-reanimated';
+import Animated, {Easing, useAnimatedStyle, useSharedValue, withTiming} from 'react-native-reanimated';
 
 export default function App() {
+    const tapSize = useSharedValue(150);
+    const longPressSize = useSharedValue(150);
+    const panTranslationX = useSharedValue(0);
+    const panTranslationY = useSharedValue(0);
+    const pinchScale = useSharedValue(1);
+
+    const config = {
+        duration: 500,
+        easing: Easing.bezier(0.5, 0, 0.5, 1)
+    };
+    const instantConfig = {
+        duration: 1,
+        easing: Easing.linear
+    };
+
+    const tapStyle = useAnimatedStyle(() => {
+        return {
+            width: withTiming(tapSize.value, config),
+            height: withTiming(tapSize.value, config)
+        }
+    });
+
+    const longPressStyle = useAnimatedStyle(() => {
+        return {
+            width: withTiming(longPressSize.value, config),
+            height: withTiming(longPressSize.value, config)
+        }
+    });
+
+    const panStyle = useAnimatedStyle(() => {
+        return {
+            transform: [
+                {
+                    translateX: withTiming(panTranslationX.value, instantConfig),
+                },
+                {
+                    translateY: withTiming(panTranslationY.value, instantConfig),
+                }
+            ]
+        }
+    });
+
+    const pinchStyle = useAnimatedStyle(() => {
+        return {
+            transform: [
+                {
+                    scale: withTiming(pinchScale.value, instantConfig),
+                },
+            ]
+        }
+    });
+
     const imageTap = Gesture.Tap()
-        .onBegin(() => console.log('Tap onBegin'))
-        .onTouchesDown(() => console.log('Tap onTouchesDown'))
-        .onTouchesMove(() => console.log('Tap onTouchesMove'))
-        .onTouchesUp(() => console.log('Tap onTouchesUp'))
-        .onTouchesCancelled(() => console.log('Tap onTouchesCancelled'))
-        .onStart(() => console.log('Tap onStart'))
-        .onEnd(() => console.log('Tap onEnd'))
-        .onFinalize(() => console.log('Tap onFinalize ----------------'));
+        .onStart(() => {
+            if (tapSize.value === 150) {
+                tapSize.value = 200;
+            } else {
+                tapSize.value = 150;
+            }
+        });
 
     const imageLongPress = Gesture.LongPress()
-        .onBegin(() => console.log('LongPress onBegin'))
-        .onTouchesDown(() => console.log('LongPress onTouchesDown'))
-        .onTouchesMove(() => console.log('LongPress onTouchesMove'))
-        .onTouchesUp(() => console.log('LongPress onTouchesUp'))
-        .onTouchesCancelled(() => console.log('LongPress onTouchesCancelled'))
-        .onStart(() => console.log('LongPress onStart'))
-        .onEnd(() => console.log('LongPress onEnd'))
-        .onFinalize(() => console.log('LongPress onFinalize ----------------'));
+        .onStart(() => {
+            if (longPressSize.value === 150) {
+                longPressSize.value = 100;
+            } else {
+                longPressSize.value = 150;
+            }
+        });
 
     const imagePan = Gesture.Pan()
-        .onBegin(() => console.log('Pan onBegin'))
-        .onTouchesDown(() => console.log('Pan onTouchesDown'))
-        .onTouchesMove(() => console.log('Pan onTouchesMove'))
-        .onTouchesUp(() => console.log('Pan onTouchesUp'))
-        .onTouchesCancelled(() => console.log('Pan onTouchesCancelled'))
-        .onStart(() => console.log('Pan onStart'))
-        .onEnd(() => console.log('Pan onEnd'))
-        .onFinalize(() => console.log('Pan onFinalize ----------------'));
+        .onUpdate((event) => {
+            panTranslationX.value = event.translationX;
+            panTranslationY.value = event.translationY;
+        })
+        .onFinalize(() => {
+            panTranslationX.value = 0;
+            panTranslationY.value = 0;
+        });
 
     const imagePinch = Gesture.Pinch()
-        .onBegin(() => console.log('Pinch onBegin'))
-        .onTouchesDown(() => console.log('Pinch onTouchesDown'))
-        .onTouchesMove(() => console.log('Pinch onTouchesMove'))
-        .onTouchesUp(() => console.log('Pinch onTouchesUp'))
-        .onTouchesCancelled(() => console.log('Pinch onTouchesCancelled'))
-        .onStart(() => console.log('Pinch onStart'))
-        .onEnd(() => console.log('Pinch onEnd'))
-        .onFinalize(() => console.log('Pinch onFinalize ----------------'));
+        .onUpdate((event) => {
+            pinchScale.value = event.scale;
+        })
+        .onFinalize(() => {
+            pinchScale.value = 1;
+        });
 
     return (
         <GestureHandlerRootView>
@@ -52,25 +100,25 @@ export default function App() {
                 <GestureDetector gesture={imageTap}>
                     <Animated.Image
                         source={{ uri: 'https://placedog.net/200/200' }}
-                        style={{ width: 150, height: 150 }}/>
+                        style={[{ width: 150, height: 150 }, tapStyle]}/>
                 </GestureDetector>
                 <View style={myTheme.spacer}/>
                 <GestureDetector gesture={imageLongPress}>
                     <Animated.Image
                         source={{ uri: 'https://placedog.net/300/300' }}
-                        style={{ width: 150, height: 150 }}/>
+                        style={[{ width: 150, height: 150 }, longPressStyle]}/>
                 </GestureDetector>
                 <View style={myTheme.spacer}/>
                 <GestureDetector gesture={imagePan}>
                     <Animated.Image
                         source={{ uri: 'https://placedog.net/400/400' }}
-                        style={{ width: 150, height: 150 }}/>
+                        style={[{ width: 150, height: 150 }, panStyle]}/>
                 </GestureDetector>
                 <View style={myTheme.spacer}/>
                 <GestureDetector gesture={imagePinch}>
                     <Animated.Image
                         source={{ uri: 'https://placedog.net/500/500' }}
-                        style={{ width: 150, height: 150 }}/>
+                        style={[{ width: 150, height: 150 }, pinchStyle]}/>
                 </GestureDetector>
             </View>
         </GestureHandlerRootView>
